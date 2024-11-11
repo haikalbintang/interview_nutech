aa6eed71c3d66bfd2e0e59d0f80e99e622594c8f
haikalbintang <mhaikalbintang.work@gmail.com>
2024-11-11 22:34:25 +0700

    First commmit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 function App() {
   return (
-    <>
-      <div className="min-h-screen w">
-        <h1 className="">TEST</h1>
+    <div className="flex justify-center items-center">
+      <div className="min-h-screen w-full max-w-[1366px]">
+        <div className="min-h-screen w-full bg-emerald-400">
+          <h1 className="flex justify-center items-center text-4xl font-bold">
+            TEST
+          </h1>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
